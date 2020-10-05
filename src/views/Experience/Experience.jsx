25aca97dc92371f2216b479484cdb0aa7cbab2bb
Someone-1243 <justinhye@gmail.com>
2020-10-04 22:46:51 -0400

    Experience

--- src/views/Experience/Experience.jsx
+++ src/views/Experience/Experience.jsx
@@ -3,30 +3,32 @@ import "./Experience.css";
 
 function TimeLineItem(props) {
     return (
-        <div className="timeline-item">
-            
-            <h1>{props.title}</h1>
+        <div className="timeline-item"
+            onClick={() => {
+                props.setActiveTitle(props.title);
+            }} >
+            <h1 className="timeline-title"> {props.title}</h1>
         </div>
     )
 }
 
 export default function ExperienceScreen() {
     const [activeTitle, setActiveTitle] = React.useState('BLANK');
-
-
-     return (
+    return (
         <div className="screen-background">
-          <div className="experience-background">
-            <div className="timeline-background">
-                <TimeLineItem title="Camp Counselor" />
-                <TimeLineItem title="Lifeguard" />
+            <div className="experience-background">
+                <div className="timeline-background">
+                    <TimeLineItem
+                        title="Camp Counselor"
+                        setActiveTitle={setActiveTitle} />
+                    <TimeLineItem
+                        title="Lifeguard"
+                        setActiveTitle={setActiveTitle}/>
+                </div>
+                <div className="highlight-background">
+                    <h1>{activeTitle}</h1>
+                </div>
             </div>
-            <div className="highlight-background">
-                <h1>{activeTitle}</h1>
-                <p>all the date information about the selected experience!</p>
-                <h4>description of the selected experience</h4>
-            </div>
-          </div>
         </div>
-      );
+    );
 }
